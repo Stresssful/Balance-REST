0184eb1e31260efa9c8ca4224b1f00fd8a8769fb
viktor.kudriavtsev <victor.kudriavtsev@blackthorn-vision.com>
2020-12-13 14:16:26 +0200

    changed db driver

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var groups = require('./routes/groups');
-var inbox = require('./routes/inbox');
-var tmp = require('./routes/tmp');
+//var groups = require('./routes/groups');
+//var inbox = require('./routes/inbox');
+//var tmp = require('./routes/tmp');
 
 var app = express();
 
@@ -27,9 +27,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/api/users', users);
-app.use('/api/groups', groups);
-app.use('/api/inbox', inbox);
-app.use('/api/tmp', tmp);
+//app.use('/api/groups', groups);
+//app.use('/api/inbox', inbox);
+//app.use('/api/tmp', tmp);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,49 @@
 var express = require('express');
 var router = express.Router();
+var mongoClient = require('mongodb').MongoClient;
+const baseUri = "mongodb+srv://admin:administrator@botdb.zctxy.azure.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const databaseName = "KursachDb";
+const usersCollectionName = "Users";
 
 var monk = require('monk');
 var db = monk('main:root@ds161148.mlab.com:61148/heroku_tqh5hdjz');
 
+var client = new mongoClient(baseUri, { useNewUrlParser: true });
+client.connect();
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  var collection = db.get('Users'); 
-  var query = { email: req.query.Email, password: req.query.Password };
-  collection.find(query, function(err, Users)
+  let collection = client.db(databaseName).collection(usersCollectionName);
+  //var collection = db.get('Users'); 
+  let query = { email: req.query.Email, password: req.query.Password };
+  collection.findOne(query, async function(err, doc){
+	if (err) throw err; 
+	res.json(Users);
+  });
+
+  /*collection.find(query, function(err, Users)
   {
   		console.log(req.query.Email);
   		console.log(req.query.Password);
   		console.log(Users);
 		if (err) throw err; 
 		res.json(Users);
-  });
+  });*/
 });
 
 router.post('/', function(req, res)
 { 
-	var collection = db.get('Users'); 
+
+	let users = client.db(databaseName).collection(usersCollectionName);
+    await users.insertOne({ "email": req.body.Email, "name": req.body.Password, "data": req.body.Data },
+        function(err, res) {
+    		if (err) throw err;
+    		console.log("1 document inserted");
+    		db.close();
+  	});
+
+	/*var collection = db.get('Users'); 
 	collection.insert(
 	{ 
 		email: req.body.Email, 
@@ -32,22 +54,39 @@ router.post('/', function(req, res)
 	{
 		if (err) throw err;
 		res.json(user);
-	});
+	});*/
 });
 
 router.get('/:id', function(req, res) 
 {
-	var collection = db.get('Users');
+
+	let collection = client.db(databaseName).collection(usersCollectionName);
+  	//var collection = db.get('Users'); 
+  	let query = { _id: req.params.id };
+  	collection.findOne(query, async function(err, doc){
+		if (err) throw err; 
+		res.json(Users);
+  	});
+
+
+	/*var collection = db.get('Users');
 	collection.findOne({ _id: req.params.id }, function(err, user)
 	{
 		if (err) throw err;
 		res.json(user);
-	});
+	});*/
 });
 
 router.put('/:id', function(req, res)
 { 
-	var collection = db.get('Users'); collection.update(
+
+	let users = client.db(databaseName).collection(usersCollectionName);
+    await users.updateOne(
+        { _id: req.params.id },
+        { $set: { "email": req.body.Email, "name": req.body.Password, "data": req.body.Data }}
+    );
+
+	/*var collection = db.get('Users'); collection.update(
 	{
 		_id: req.params.id
 	},
@@ -62,7 +101,7 @@ router.put('/:id', function(req, res)
 	{
 		if (err) throw err;
 		res.json(user);
-	});
+	});*/
 });
 
 module.exports = router;
